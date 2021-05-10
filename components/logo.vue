218c40bccce1c711edb903ee14c1e7196e8aab4e
BartKnucle <sjarde@yahoo.fr>
2021-05-10 09:18:10 +0200

    Add logo

---
+++ components/logo.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-img
+    class="Logo"
+    alt="Funky Sheep Logo"
+    src="/img/Logo-Mini.png"
+  />
+</template>
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      default: '/img/logo.png'
+      }
+    }
+  }
+</script>
