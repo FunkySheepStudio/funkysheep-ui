38f2acca0168d614e208eed5ca716cbc93b24676
BartKnucle <sjarde@yahoo.fr>
2021-05-11 18:05:44 +0200

    Add social

---
+++ components/social.vue
@@ -0,0 +1,42 @@
+<template>
+  <section>
+    <v-btn
+      :hidden="!$vuetify.breakpoint.mobile"
+      fab
+      elevation="0"
+      x-small
+      color="primary"
+      @click="mobileMenu = !mobileMenu"
+    >
+      <v-icon dark>
+        mdi-menu
+      </v-icon>
+    </v-btn>
+    <v-btn
+      :hidden="!$vuetify.breakpoint.mobile"
+      fab
+      elevation="0"
+      x-small
+      color="primary"
+      @click="mobileMenu = !mobileMenu"
+    >
+      <v-icon dark>
+        mdi-discord
+      </v-icon>
+    </v-btn>
+  </section>
+</template>
+<script>
+export default {
+  props:{
+    src: {
+      type: String,
+      default: '/img/logo.png'
+    },
+    to: {
+      type: String,
+      default: '/'
+    }
+  }
+}
+</script>
